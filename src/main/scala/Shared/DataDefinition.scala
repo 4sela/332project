8e7038affbdfc14ed3556ccc1fbb301b33404df0
Gousse-Gousse <adresse.poubellevt@gmail.com>
2025-12-07 21:47:06 +0900

    addinng the first version of the merging part

--- src/main/scala/Shared/DataDefinition.scala
+++ src/main/scala/Shared/DataDefinition.scala
@@ -18,7 +18,7 @@ type DataStream = LazyList[DataArray]
 type Path = String
 type JavaPath = java.nio.file.Path
 
-
+type MergingStructure = List[LazyList[KeyValueArray]]
 
 object KeyValueArray:
 	val SIZE: Int = Key.SIZE + Value.SIZE
@@ -29,16 +29,24 @@ object Key:
 object Value:
 	val SIZE: Int = 90
 
+
 object DataStream:
 	val NUMBER_OF_KEYVALUE_PER_CHUNK: Int = 1000
 	val SIZE_CHUNK: Int = NUMBER_OF_KEYVALUE_PER_CHUNK * KeyValueArray.SIZE
 
+
+
+object COMMON_VALUE:
+  /**aimed number of key_value poer final files.*/
+  val SIZE_REGULAR_FINAL_FILE: Int = 10_000
+
+
+
+
 object MyOrdering extends Ordering[KeyValueArray]:
-	override def compare(x: KeyValueArray, y: KeyValueArray): Int =
-		x.getKey.zip(y.getKey).find(_!=_).map((a,b) => if(a>b) 1 else -1).getOrElse(0)
-
-
-
+	override def compare(x: KeyValueArray, y: KeyValueArray): Int = {
+    java.util.Arrays.compareUnsigned(x, 0, Key.SIZE, y, 0, Key.SIZE)
+  }
 
 
 
@@ -64,20 +72,36 @@ extension (array: DataArray)
 
 
 extension (path: Path)
-	def getPath: java.nio.file.Path =
-		java.nio.file.Paths.get(path)
-	
-	def getPathPlusName(name: String): java.nio.file.Path =
-		java.nio.file.Paths.get(path,name)
-	
-	def read(read_full_file: Boolean = true): Option[DataArray | DataStream] =
-		IO_OPERATION.read(path, read_full_file)
-	
-	def read_full: Option[DataArray] =
-		IO_OPERATION.read_full(path)
-	
-	def read_slow: Option[DataStream] =
-		IO_OPERATION.read_slow(path)
+  def getPath: java.nio.file.Path = {
+    java.nio.file.Paths.get(path)
+  }
+
+  def getPathPlusName(name: String): java.nio.file.Path = {
+    java.nio.file.Paths.get(path, name)
+  }
+
+  def read(read_full_file: Boolean = true): Option[DataArray | DataStream] = {
+    IO_OPERATION.read(path, read_full_file)
+  }
+
+  def read_full: Option[DataArray] = {
+    IO_OPERATION.read_full(path)
+  }
+
+  def write_if_directory(name: String, dataArray: DataArray): Option[Path] = {
+    IO_OPERATION.create_file(path, name) match {
+      case None => None
+      case Some(file_path) =>
+        IO_OPERATION.write(dataArray,file_path)
+    }
+  }
+
+
+  def read_slow: Option[DataStream] ={
+    IO_OPERATION.read_slow(path)
+  }
+
+
 
 
 extension (path: JavaPath)
@@ -96,75 +120,117 @@ extension (stream: DataStream)
 		stream.flatMap(_.sliding(KeyValueArray.SIZE, KeyValueArray.SIZE).toList)
 
 
+extension (inputs: List[LazyList[KeyValueArray]])
+  def mergeAllSorted: LazyList[KeyValueArray] = {
+    val activeStreams = inputs.filter(_.nonEmpty)
+    if (activeStreams.isEmpty) {
+      LazyList.empty
+    } else {
+      val (minStream, index) = activeStreams.zipWithIndex.
+        minBy{case (stream, _) => stream.head}(using MyOrdering)
+      minStream.head #:: activeStreams.updated(index, minStream.tail).mergeAllSorted
+    }
+  }
+
+
 
 
 object IO_OPERATION:
-	def read(path: Path, read_full_file: Boolean = true): Option[DataArray | DataStream] =
-		if (read_full_file) read_full(path) else read_slow(path)
-	
-	def read_full(path: Path): Option[DataArray] =
-		val file = new BufferedInputStream(new FileInputStream(path))
-		try {
-			val return_value: DataArray = file.readAllBytes()
-			Some(return_value)
-		} catch {
-			case _: Throwable => Option.empty
-		} finally {
-			file.close()
-		}
-	
-	
-	//TODO not shure of this one (in worse case merge 2 file per 2 file in a pyramide order. Awfull in theory but easer iG)
-	def read_slow(path: Path): Option[DataStream] =
-		val file = new BufferedInputStream(new FileInputStream(path))
-		try {
-			def list_creation: DataStream =
-				if (file.available() <= DataStream.SIZE_CHUNK) file.readAllBytes() #:: LazyList.empty
-				else file.readNBytes(DataStream.SIZE_CHUNK) #:: list_creation
-			
-			Some(list_creation)
-			
-		} catch {
-			case _: Throwable => Option.empty
-		} finally {
-			file.close()
-		}
-	
-	
-	//this one should work IG
-	def write(data: DataArray, path: Path): Option[Path] =
-		val file = new BufferedOutputStream(new FileOutputStream(path))
-		try {
-			file.write(data)
-			Some(path)
-		} catch {
-			case _: Throwable => Option.empty
-		} finally {
-			file.close()
-		}
-	
-	
-	def scan_directory_name(path: Path): Option[List[Path]] =
-		try {
-			val full_path = path.getPath
-			if (full_path.exist && full_path.isDirectory) {
-				Some(
-					java.nio.file.Files.list(full_path).map(_.toString).toList.asScala.toList
-				)
-			}else {
-				return None
-			}
-		} catch {
-			case e: Exception =>
-				println(s"fail to read the directory. Error : ${e.getMessage}")
-				return None
-		}
-	
-	//how_many_strat_to_explore <0 ---> explore all of the file system from here
-	def print_all_file_name(directory: Path)
-	                       (information_to_print: JavaPath => String = _.toString,
-	                        way_to_print: String => Unit = println,
-	                        how_many_strat_to_explore: Int = 2): Unit = {
+  def read(path: Path, read_full_file: Boolean = true): Option[DataArray | DataStream] = {
+    if (read_full_file) read_full(path) else read_slow(path)
+  }
+
+  def read_full(path: Path): Option[DataArray] = {
+    val file = new BufferedInputStream(new FileInputStream(path))
+    try {
+      val return_value: DataArray = file.readAllBytes()
+      Some(return_value)
+    } catch {
+      case _: Throwable => Option.empty
+    } finally {
+      file.close()
+    }
+  }
+
+  def read_slow(path: Path): Option[DataStream] = {
+    try {
+      val file = new BufferedInputStream(new FileInputStream(path))
+      val iterator: Iterator[DataArray] = new Iterator[DataArray] {
+        var nextChunk: Option[DataArray] = fetchNext()
+
+        def fetchNext(): Option[DataArray] =
+          try {
+            val buffer = new Array[Byte](DataStream.SIZE_CHUNK)
+            val bytesRead = file.readNBytes(buffer, 0, DataStream.SIZE_CHUNK)
+
+            if (bytesRead > 0) {
+              if (bytesRead < DataStream.SIZE_CHUNK) {
+                Some(buffer.take(bytesRead))
+              }
+              else {
+                Some(buffer)
+              }
+            } else {
+              close()
+              None
+            }
+          } catch {
+            case _: Throwable => {
+              close()
+              None
+            }
+          }
+
+        def close(): Unit = file.close()
+
+        override def hasNext: Boolean = nextChunk.isDefined
+
+        override def next(): DataArray =
+          val res = nextChunk.get
+          nextChunk = fetchNext()
+          res
+      }
+
+      Some(LazyList.from(iterator))
+
+    } catch {
+      case _: Throwable => None
+    }
+  }
+
+  def write(data: DataArray, path: Path): Option[Path] = {
+    val file = new BufferedOutputStream(new FileOutputStream(path))
+    try {
+      file.write(data)
+      Some(path)
+    } catch {
+      case _: Throwable => Option.empty
+    } finally {
+      file.close()
+    }
+  }
+
+  def scan_directory_name(path: Path): Option[List[Path]] = {
+    try {
+      val full_path = path.getPath
+      if (full_path.exist && full_path.isDirectory) {
+        Some(
+          java.nio.file.Files.list(full_path).map(_.toString).toList.asScala.toList
+        )
+      } else {
+        return None
+      }
+    } catch {
+      case e: Exception =>
+        println(s"fail to read the directory. Error : ${e.getMessage}")
+        return None
+    }
+  }
+	//how_many_strat_to_explore <0 => explore all of the file system from here
+  def print_all_file_name(directory: Path)(
+    information_to_print: JavaPath => String = _.toString,
+    way_to_print: String => Unit = println,
+    how_many_strat_to_explore: Int = 2): Unit = {
 		
 		def print_with_javaPath(path: JavaPath)(num_strat_remaining: Int = how_many_strat_to_explore): Unit = {
 			if (path.exist && num_strat_remaining!=0)
@@ -181,11 +247,12 @@ object IO_OPERATION:
 				println(s"error while printing a directory at : \"$directory\" with error : ${e.getMessage}")
 		}
 	}
-	
-	def print_all_file_name(directory: Path): Unit =
-		print_all_file_name(directory)()
-	
-	def create_directory(path: Path, name: String): Option[Path] = {
+
+  def print_all_file_name(directory: Path): Unit = {
+    print_all_file_name(directory)()
+  }
+
+  def create_directory(path: Path, name: String): Option[Path] = {
 		try {
 			val full_path = path.getPathPlusName(name)
 			if (java.nio.file.Files.exists(full_path) && java.nio.file.Files.isDirectory(full_path)) {
@@ -202,9 +269,8 @@ object IO_OPERATION:
 				None
 		}
 	}
-	
-	
-	def create_file(directory: Path, name: String): Option[Path] = {
+
+  def create_file(directory: Path, name: String): Option[Path] = {
 		try {
 			val full_path = directory.getPathPlusName(name)
 			if (java.nio.file.Files.exists(full_path)) {
@@ -221,9 +287,8 @@ object IO_OPERATION:
 				None
 		}
 	}
-	
-	
-	def delete_single_file(path: Path): Unit = {
+
+  def delete_single_file(path: Path): Unit = {
 		try {
 			val full_path = path.getPath
 			if (!java.nio.file.Files.exists(full_path)){
@@ -240,8 +305,8 @@ object IO_OPERATION:
 				println(s"Fail to delete the file at :  $path. Because of ${e.getMessage}")
 		}
 	}
-	
-	def delete_single_directory(path: Path): Unit = {
+
+  def delete_single_directory(path: Path): Unit = {
 		try {
 			val full_path = java.nio.file.Paths.get(path)
 			if (!java.nio.file.Files.exists(full_path)){
@@ -258,9 +323,8 @@ object IO_OPERATION:
 				println(s"Fail to delete the directory at :  $path. Because of ${e.getMessage}")
 		}
 	}
-	
-	
-	def delete_fully_path(path: Path): Unit = {
+
+  def delete_fully_path(path: Path): Unit = {
 		try {
 			val full_path = path.getPath
 			if (!full_path.exist){
@@ -283,21 +347,14 @@ object IO_OPERATION:
 	
 	
 	/** ++++++ assume that there is only the inputfile/tempfile in the given directory ++++++ */
-	def slow_read_all_file_parallel(directory_path: Path): List[DataStream] =
-		scan_directory_name(directory_path).get.map(read_slow).map(_.getOrElse(LazyList.empty)).filter(_.nonEmpty)
-	
-	
-	//TODO check if it works properly !!!
-	def slow_read_all_file_in_raw(directory_path: Path): DataStream =
-		LazyList.from(scan_directory_name(directory_path).get).map(read_slow).map(_.getOrElse(LazyList.empty)).flatMap(_.toArray)
+  def slow_read_all_file_parallel(directory_path: Path): List[DataStream] = {
+    scan_directory_name(directory_path).get.map(read_slow).map(_.getOrElse(LazyList.empty)).filter(_.nonEmpty)
+  }
 
+  def get_lazy_sorted_list(directory_path: Path): LazyList[KeyValueArray] = {
+    slow_read_all_file_parallel(directory_path).mergeAllSorted
+  }
 
-
-
-
-object k:
-	@main
-	def main(): Unit = {
-		val d = IO_OPERATION.create_directory("./", "YES")
-		val file = d.flatMap(IO_OPERATION.create_file(_,"yes"))
-	}
+  def slow_read_all_file_in_raw(directory_path: Path): DataStream = {
+    LazyList.from(scan_directory_name(directory_path).get).map(read_slow).map(_.getOrElse(LazyList.empty)).flatMap(_.toArray)
+  }
--- src/main/scala/Worker/MergeThread.scala
+++ src/main/scala/Worker/MergeThread.scala
@@ -1,5 +1,19 @@
 package Worker
 
-case class MergeThread() extends Thread{
-	override def run(): Unit = ???
+import Shared.*
+
+
+case class MergeThread(temporary_directory: Path, final_directory: Path = "./final_directory") extends Thread{
+	override def run(): Unit = {
+    var lazy_sorted_list = IO_OPERATION.get_lazy_sorted_list(temporary_directory)
+    var index: Int = 0
+    while (lazy_sorted_list.nonEmpty){
+      val (chunks,tail) = lazy_sorted_list.splitAt(COMMON_VALUE.SIZE_REGULAR_FINAL_FILE)
+      val data_array = chunks.toList.flatten.toArray
+      val final_files = final_directory.write_if_directory("final_file_"+index.toString, data_array)
+      //notice if Option == None -> error
+      index += 1
+      lazy_sorted_list = tail
+    }
+  }
 }
